afa77acde0704d9913bda0133ce69eaf083d474d
Rituparna W <rituparnaw16@gmail.com>
2026-03-15 14:10:36 +0100

    lib(uart): implement hardware initialization and data FIFO checks
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,10 @@
 #include "lib/uart/uart.h"
 
 void kernel_main() {
-  char *hello = "Hello !";
-
-  uart_puts(hello);
+  uart_init();
+  uart_println("Fermi OS - Booting up !");
 
   while (1) {
+    uart_putc(uart_getc());
   }
 }
--- src/lib/uart/uart.h
+++ src/lib/uart/uart.h
@@ -1,10 +1,21 @@
 #ifndef LIB_UART_H
 #define LIB_UART_H
 
+#include <stdint.h>
 #define UART_BASE 0x9000000
 #define UART_DR (UART_BASE + 0x00)
+#define UART_FR (UART_BASE + 0x18)
+#define UART_IBRD (UART_BASE + 0x24)
+#define UART_FBRD (UART_BASE + 0x28)
+#define UART_LCRH (UART_BASE + 0x2C)
+#define UART_CR (UART_BASE + 0x30)
+#define UART_ICR (UART_BASE + 0x44)
+
+void uart_init(void);
 
 void uart_putc(const char c);
+uint8_t uart_getc(void);
 void uart_puts(const char *str);
+void uart_println(const char *str);
 
 #endif // !LIB_UART_H

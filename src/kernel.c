2b666c66252c46f96981ec9ce5fb3a6798b1acc4
Rituparna W <rituparnaw16@gmail.com>
2026-03-24 16:03:56 +0100

    feat(exception-level): fetch and print current exception levels
    Tested all exception levels by toggling virtualization and secure flags
    
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,11 @@
 #include "uart/uart.h"
+#include "utils/utils.h"
 
 void kernel_main() {
   uart_init();
 
   uart_println("Fermi OS - Booting Up...");
+  print_current_el();
 
   while (1) {
     uart_putc(uart_getc());

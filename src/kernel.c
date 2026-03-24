e824dc25a27ec5c914e716a6ce1a328c9a910efd
Rituparna W <rituparnaw16@gmail.com>
2026-03-24 19:39:26 +0100

    chore(pmm): remove logs and add print_info function
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -11,23 +11,16 @@ void kernel_main() {
 
   pmm_init(MEM_START, MEM_SIZE);
 
+  pmm_print_info();
+
   uintptr_t addr1 = pmm_allocate_page();
-  uart_puthex(addr1);
-  uart_println("");
-
   uintptr_t addr2 = pmm_allocate_page();
-  uart_puthex(addr2);
-  uart_println("");
-  pmm_free_page(addr2);
 
-  uintptr_t addr3 = pmm_allocate_page();
-  uart_puthex(addr3);
-  uart_println("");
-  // not double free... addr3 got reassigned to addr2
+  pmm_print_info();
   pmm_free_page(addr2);
+  pmm_print_info();
   pmm_free_page(addr1);
-  // error here
-  pmm_free_page(addr3);
+  pmm_print_info();
 
   while (1) {
     uart_putc(uart_getc());
--- src/pmm/pmm.c
+++ src/pmm/pmm.c
@@ -26,6 +26,34 @@ static inline int bitmap_test(uint64_t pfn) {
   return (bitmap[BITMAP_INDEX(pfn)] >> BITMAP_BIT(pfn)) & 1;
 }
 
+void pmm_print_info() {
+  uint64_t mem_size = mem_region_end - mem_region_start;
+
+  uart_puts("[PMM][INFO] Memory region: ");
+  uart_puthex(mem_region_start);
+  uart_puts(" - ");
+  uart_puthex(mem_region_end);
+  uart_println("");
+
+  uart_puts("[PMM][INFO] Memory Size: ");
+  uart_puthex(mem_size);
+  uart_puts(" | ");
+  uart_putdec(mem_size/1024/1024);
+  uart_println(" mbytes");
+
+  uart_puts("[PMM][INFO] Total Pages: ");
+  uart_putdec(total_pages);
+  uart_println("");
+
+  uart_puts("[PMM][INFO] Reserved Pages: ");
+  uart_putdec(reserved_pages);
+  uart_println("");
+
+  uart_puts("[PMM][INFO] Used Pages: ");
+  uart_putdec(used_pages);
+  uart_println("");
+}
+
 void pmm_init(uintptr_t mem_start, uint64_t mem_size) {
   uart_println("[PMM] Initializing Physical Memory Manager");
   mem_region_start = mem_start;
@@ -33,48 +61,30 @@ void pmm_init(uintptr_t mem_start, uint64_t mem_size) {
 
   total_pages = mem_size / PAGE_SIZE;
 
-  uart_puts("[PMM] Memory Starts: ");
-  uart_puthex(mem_region_start);
-  uart_println("");
-
-  uart_puts("[PMM] Memory Ends: ");
-  uart_puthex(mem_region_end);
-  uart_println("");
-
-  uart_puts("[PMM] Memory Size: ");
-  uart_puthex(mem_size);
-  uart_puts(" | ");
-  uart_putdec(mem_size);
-  uart_println(" bytes");
-
-  uart_puts("[PMM] Total Pages: ");
-  uart_putdec(total_pages);
-  uart_println("");
-
   // one bit per page.
   // need ceil(total_pages / 64) uint64_t entries
   bitmap_size = (total_pages + 63) / 64;
   uint64_t bitmap_bytes = bitmap_size * sizeof(uint64_t);
 
-  uart_puts("[PMM] Bitmap Length: ");
-  uart_putdec(bitmap_size);
-  uart_println("");
+  // uart_puts("[PMM] Bitmap Length: ");
+  // uart_putdec(bitmap_size);
+  // uart_println("");
 
-  uart_puts("[PMM] Bitmap Bytes: ");
-  uart_putdec(bitmap_bytes);
-  uart_println("");
+  // uart_puts("[PMM] Bitmap Bytes: ");
+  // uart_putdec(bitmap_bytes);
+  // uart_println("");
 
   // Place bitmap at the first page-aligned address after the kernel
   uint64_t kernel_end = (uint64_t)&__kernel_end;
   bitmap = (uint64_t *)PAGE_ALIGN_UP(kernel_end);
 
-  uart_puts("[PMM] Bitmap address: ");
-  uart_puthex((uintptr_t)bitmap);
-  uart_println("");
+  // uart_puts("[PMM] Bitmap address: ");
+  // uart_puthex((uintptr_t)bitmap);
+  // uart_println("");
 
-  uart_puts("[PMM] Kernel End: ");
-  uart_puthex(kernel_end);
-  uart_println("");
+  // uart_puts("[PMM] Kernel End: ");
+  // uart_puthex(kernel_end);
+  // uart_println("");
 
   uart_println("[PMM] Zeroing Bitmap");
   memset(bitmap, 0, bitmap_bytes);
@@ -85,13 +95,9 @@ void pmm_init(uintptr_t mem_start, uint64_t mem_size) {
   uint64_t reserved_end = PAGE_ALIGN_UP(bitmap_end);
   reserved_pages = (reserved_end - mem_region_start) / PAGE_SIZE;
 
-  uart_puts("[PMM] Bitmap End: ");
-  uart_puthex(reserved_end);
-  uart_println("");
-
-  uart_puts("[PMM] Reserved Pages: ");
-  uart_putdec(reserved_pages);
-  uart_println("");
+  // uart_puts("[PMM] Bitmap End: ");
+  // uart_puthex(reserved_end);
+  // uart_println("");
 
   // mark reserverd pages
   for (uint64_t pfn = 0; pfn < reserved_pages; pfn++) {
